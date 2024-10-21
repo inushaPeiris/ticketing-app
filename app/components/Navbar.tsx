3958513bfae8e384e9c94fd55613b57b7c979407
inushapeiris <inushaumayanthapeiris@gmail.com>
2024-10-22 00:11:04 +0530

    minor

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,13 +11,12 @@ interface NavbarProps {
 
 export default function Navbar({ onShow, darkMode }: NavbarProps) {
   return (
-    <nav>
+    <nav className="relative">
       <Image src={Logo} alt="iCare Helpdesk logo" width={70} placeholder="blur" quality={100} />
       <h1>iCare Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
-      {darkMode ? <button onClick={onShow}>Light 🌞</button> : <button onClick={onShow}>Dark 🌜</button>}
-      <div>Mode: {darkMode ? "dark" : "light"}</div>
+      <div className="absolute right-4">{darkMode ? <button onClick={onShow}>Light 🌞</button> : <button onClick={onShow}>Dark 🌜</button>}</div>
     </nav>
   );
 }
